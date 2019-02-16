20765e831f3022ca3ce28254baeb50d080c24130
Alex DeLorenzo <alex.delorenzo@gmail.com>
2019-02-16 17:28:45 -0500

    add tags endpoint

---
+++ app/recipe/tests/test_tags_api.py
@@ -0,0 +1,59 @@
+from django.contrib.auth import get_user_model
+from django.urls import reverse
+from django.test import TestCase
+
+from rest_framework import status
+from rest_framework.test import APIClient
+
+from core.models import Tag
+from recipe.serializers import TagSerializer
+
+
+TAGS_URL = reverse('recipe:tag-list')
+
+
+class PublicTagApiTests(TestCase):
+    """Test the public Tags API"""
+    def setUp(self):
+        self.client = APIClient()
+
+    def test_login_required(self):
+        response = self.client.get(TAGS_URL)
+        self.assertEqual(response.status_code, status.HTTP_401_UNAUTHORIZED)
+
+
+class PrivateTagApiTests(TestCase):
+    """Test the authorized user tags API"""
+    def setUp(self):
+        self.user = get_user_model().objects.create_user(
+            'test@test.com',
+            'test123123'
+        )
+        self.client = APIClient()
+        self.client.force_authenticate(self.user)
+
+    def test_retrieve_tags(self):
+        """Test retrieving tags"""
+        Tag.objects.create(user=self.user, name='Vegan')
+        Tag.objects.create(user=self.user, name='Dessert')
+
+        response = self.client.get(TAGS_URL)
+        tags = Tag.objects.all().order_by('-name')
+        serializer = TagSerializer(tags, many=True)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(response.data, serializer.data)
+
+    def test_tags_limited_to_user(self):
+        """Test that tags returned belong only to authenticated user"""
+        user = get_user_model().objects.create_user(
+            'test2@test.com',
+            'test123123'
+        )
+        Tag.objects.create(user=user, name='Citrus')
+        tag = Tag.objects.create(user=self.user, name='Comfort')
+
+        response = self.client.get(TAGS_URL)
+
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(len(response.data), 1)
+        self.assertEqual(response.data[0].name, 'Comfort')
